813297428bdb0e2b4bbc41d52769a0533ac9b234
Hitjack007 <m43222783@gmail.com>
2025-07-01 16:56:07 +1200

    Add files via upload

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mark Greene on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
